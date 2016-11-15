fec57e7fa6772cfb0c8a3c2a683301042cc9aba6
Jose <jose.julcarima@dev4media.pe>
2016-11-15 12:18:31 -0500

    termine el refresh indicator :)

--- app/src/main/java/thefenix/darkblack/petagram/MainActivity.java
+++ app/src/main/java/thefenix/darkblack/petagram/MainActivity.java
@@ -1,17 +1,50 @@
 package thefenix.darkblack.petagram;
 
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    SwipeRefreshLayout sfiMiIndicadoRefresh;
+    ListView lstMiLista;
+    Adapter adaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         agregarFAB();
+
+        sfiMiIndicadoRefresh = (SwipeRefreshLayout) findViewById(R.id.sfiMiIndicadoRefresh);
+        lstMiLista =(ListView) findViewById(R.id.lstMiLista);
+
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+        lstMiLista.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, planetas));
+
+        sfiMiIndicadoRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+            }
+        });
+
+
+    }
+
+    public void refrescandoContenido(){
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+        lstMiLista.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, planetas));
+        sfiMiIndicadoRefresh.setRefreshing(false);
     }
 
     public void agregarFAB(){
@@ -19,6 +52,17 @@ public class MainActivity extends AppCompatActivity {
         miFAB.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+
+                Snackbar.make(v, getResources().getString(R.string.mensaje), Snackbar.LENGTH_LONG)
+                        .setAction(getResources().getString(R.string.texto_accion), new View.OnClickListener(){
+                            @Override
+                            public void onClick(View v) {
+                                Log.i("SNACKBAR", "Click en Snackbar");
+                            }
+                        })
+                        .setActionTextColor(getResources().getColor(R.color.colorPrimary))
+                        .show();
+
 
             }
         });
